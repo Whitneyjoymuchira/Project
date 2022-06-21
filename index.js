4d487f889c620539d675cec2e0431e6046a3902a
Whitney Muchira <whitneymuchira@gmail.com>
2022-06-21 21:58:10 +0300

    created files

---
+++ index.js
@@ -0,0 +1,16 @@
+
+path = 'https://api.edamam.com/search'
+const app_key = '4965fdc1aac73e6ce40c7d70ef661d48'
+
+const app_id = '0a0479e6'
+const urlLink = 'https://api.edamam.com/search?&app_id=0a0479e6&app_key=4965fdc1aac73e6ce40c7d70ef661d48&q=pizza'
+const fetchUrl = "https://api.edamam.com/search?&app_id=0a0479e6&app_key=4965fdc1aac73e6ce40c7d70ef661d48&q=pizza"
+function displayFoods() {
+    fetch(fetchUrl)
+        .then(res => res.json())
+        .then(data => data.forEach((newData) => {
+            console.log(newData.name)
+
+        }))
+}
+console.log(displayFoods)
